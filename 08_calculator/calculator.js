a3ac91c17934c440ac03ee535167b2f3a497d84b
Chezz-B <brujic.caslav@gmail.com>
2022-05-30 02:50:14 +0200

    ..

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+"use strict";
+
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0;
+  arr.forEach(n => result += n);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(...n) {
+  // console.log(n[0][0]);
+  let result = 1;
+  const arr = n[0];
+  for(let i = 0; i < arr.length; i++){
+    result *= arr[i]
+  };
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if(x == 0) return 1;
+	let result = x;
+  for(let i = 1; i < x; i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const strNoSpace = str.replaceAll(" ", "").replaceAll(".", "").replaceAll(",", "").replaceAll("!", "");
 
+  const arr = strNoSpace.toLowerCase().split("");
+  const reverseArr = arr.slice().reverse();
+  
+  if(arr.join() == reverseArr.join()) return true
+  else return false;
 };
 
 // Do not edit below this line
